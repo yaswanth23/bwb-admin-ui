5e20f736867f7f0c180a50a9aa43f4bfab84116b
yash <yashwanth.k@mooo.farm>
2023-09-09 13:14:24 +0530

    testing final run

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Fragment } from "react";
 const App = () => {
   console.log(process.env.NODE_ENV)
   console.log(process.env.REACT_APP_API_URL)
+  
   return (
     <Fragment>
       <div>hi there</div>
